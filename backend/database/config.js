abd4c5a9b5f3f0e0080df93f7a59e2bd537fe3b8
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2021-02-02 20:55:58 -0600

    DB Config

--- backend/database/config.js
+++ backend/database/config.js
@@ -8,7 +8,7 @@ const dbConnection = async() => {
             useUnifiedTopology: true,
             useCreateIndex: true
         });
-        console.log('DB Online');
+        console.log('DB Online on Port: ', process.env.DB_CNN);
     } catch (error) {
         console.log(error);
         throw new Error('Error a la hora de inicializar la BD');
